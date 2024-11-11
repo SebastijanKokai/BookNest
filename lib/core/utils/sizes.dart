5c2aca7510800f9bfc7428f64ce9674db9b6a84c
Sebastijan Kokai <sebastijan.kokai@outlook.com>
2024-11-11 17:19:40 +0100

    Add Dimens class to reuse same sizes in the app like 4, 8, 12, etc.

---
+++ lib/core/utils/sizes.dart
@@ -0,0 +1,11 @@
+typedef D = Dimens;
+
+class Dimens {
+  static const double dp4 = 4.0;
+  static const double dp8 = 8.0;
+  static const double dp12 = 12.0;
+  static const double dp16 = 16.0;
+  static const double dp20 = 20.0;
+  static const double dp24 = 24.0;
+  static const double dp32 = 32.0;
+}
