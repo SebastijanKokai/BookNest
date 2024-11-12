64a5ca1286045d73f925f70ee053596c5c14a50d
Sebastijan Kokai <sebastijan.kokai@outlook.com>
2024-11-12 20:01:50 +0100

    Register BookCubit to service_locator and provide it to DashboardPage

--- lib/features/dashboard/book_service_locator.dart
+++ lib/features/dashboard/book_service_locator.dart
@@ -2,10 +2,12 @@ import 'package:book_nest/core/utils/service_locator.dart';
 import 'package:book_nest/features/dashboard/data/provider/book_provider.dart';
 import 'package:book_nest/features/dashboard/data/repository/book_repository.dart';
 import 'package:book_nest/features/dashboard/domain/usecase/book_usecase.dart';
+import 'package:book_nest/features/dashboard/presentation/bloc/book_cubit.dart';
 
 void registerBookDependencies() {
   getIt
     ..registerLazySingleton(BookProvider.new)
     ..registerLazySingleton(() => BookRepository(bookProvider: getIt()))
-    ..registerLazySingleton(() => BookUsecase(bookRepository: getIt()));
+    ..registerLazySingleton(() => BookUsecase(bookRepository: getIt()))
+    ..registerFactory(() => BookCubit(bookUsecase: getIt()));
 }
--- lib/features/dashboard/presentation/dashboard_page.dart
+++ lib/features/dashboard/presentation/dashboard_page.dart
@@ -1,12 +1,27 @@
+import 'package:book_nest/core/utils/service_locator.dart';
+import 'package:book_nest/features/dashboard/presentation/bloc/book_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => getIt<BookCubit>()..getBooks(),
+      child: const DashboardView(),
+    );
+  }
+}
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return const Center(
-      child: Text("Dashboard page."),
+      child: Text('Dashboard page.'),
     );
   }
 }
