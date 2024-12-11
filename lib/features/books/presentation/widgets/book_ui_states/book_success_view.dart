6fc26f0626a845fa25708c77d90b5b2e479cdabb
Sebastijan Kokai <sebastijan.kokai@outlook.com>
2024-12-11 20:59:03 +0100

    Wrap _BookCard with Skeleton.leaf

--- lib/features/books/presentation/widgets/book_ui_states/book_success_view.dart
+++ lib/features/books/presentation/widgets/book_ui_states/book_success_view.dart
@@ -1,6 +1,7 @@
 import 'package:book_nest/core/utils/sizes.dart';
 import 'package:book_nest/features/books/domain/entity/book/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class BookSuccessView extends StatelessWidget {
   const BookSuccessView({required this.books, super.key});
@@ -30,18 +31,20 @@ class _BookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        color: Colors.blueGrey,
-      ),
-      child: Column(
-        children: [
-          Text('${book.author.firstName} ${book.author.lastName}'),
-          const Spacer(),
-          Text(book.title),
-        ],
+    return Skeleton.leaf(
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.blueGrey,
+        ),
+        child: Column(
+          children: [
+            Text('${book.author.firstName} ${book.author.lastName}'),
+            const Spacer(),
+            Text(book.title),
+          ],
+        ),
       ),
     );
   }
